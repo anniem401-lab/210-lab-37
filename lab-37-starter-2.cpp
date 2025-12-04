2ef912381ee8160a0a387fd1bdf7855f5cf2e911
Annie <annmora21@gmail.com>
2025-12-04 05:09:45 -0800

    Update

--- lab-37-starter-2.cpp
+++ lab-37-starter-2.cpp
@@ -1,17 +1,37 @@
+// COMSC-210 | Lab 37 | Annie Morales
+// IDE used: Visual Studio Code
+
 #include <iostream>
 using namespace std;
 
+// Function prototype
+int sum_ascii(string s);
 
 int main() {
+    /*
     char a = 'A';
-    cout << a << endl;
+    cout << a << endl; // ASCII: 65
     cout << (int) a << endl;
-    int b = 66;
+    int b = 66; // ASCII
     cout << b << endl;
     cout << (char) b << endl;
-    
+    */
+    cout << "\nReading string...\n"; 
+    string code = "Test";
 
+    sum_ascii(code);
+
+    int result = sum_ascii(code);
+    cout << "Sum of string's ASCII values: " << result << endl;
     return 0;
+}
+
+int sum_ascii(string s) {
+    int sum = 0;
+    for (int i = 0; i < s.length(); i++) {
+        sum = sum + s[i]; // ASCII value is calculated
+    }
+    return sum; // Total is returned
 }
 
 /* 
